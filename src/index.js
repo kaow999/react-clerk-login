29e4cd6961689b9a799c96bd59f803d84367ba5c
9 <devillkaow@gmail.com>
2022-07-04 12:11:01 +0700

    delete utils

--- src/index.js
+++ src/index.js
@@ -4,12 +4,27 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { firebaseConfig } from "./utils/firebaseConfig";
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBQIPLVloBll_0D9bfCZQB0X7dW0Oas5X0",
+  authDomain: "login-clerk.firebaseapp.com",
+  projectId: "login-clerk",
+  storageBucket: "login-clerk.appspot.com",
+  messagingSenderId: "684162019034",
+  appId: "1:684162019034:web:74c235808595adc01dbfe3",
+  measurementId: "G-RDGMR3VGHH"
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-getAnalytics(app);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
